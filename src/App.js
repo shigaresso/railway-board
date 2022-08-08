f289e4a1c6b2b939edf017fca039470968129024
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-09 01:19:42 +0900

    トップページに掲示板のスレッドを表示するようにした。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useFetchApi } from './hooks/useFetchApi';
+import { Thread } from "./components/Thread";
 
 function App() {
+  const threads = useFetchApi();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {threads.map(thread => <Thread key={thread.id} title={thread.title} />)}
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
