f8c9983f8f1edbb97bca80e440fc665258be1e9d
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-18 03:28:53 +0900

    API GET 接続時のエラー処理の追加

--- src/hooks/useFetchApi.js
+++ src/hooks/useFetchApi.js
@@ -5,9 +5,16 @@ export const useFetchApi = () => {
 
   useEffect(() => {
     fetch('https://railway-react-bulletin-board.herokuapp.com/threads', { method: 'GET' })
-      .then(res => res.json())
+      .then(res => {
+        if (!res.ok) throw new Error(res.status);
+        return res.json();
+      })
       .then(data => {
-        setThreads(data)
+        setThreads(data);
+      })
+      .catch(error => {
+        if (error.message.charAt(0) == 4) alert("無効な URLです");
+        if (error.message.charAt(0) == 5) alert("サーバー通信でエラーが発生しました。");
       });
   }, []);
 
