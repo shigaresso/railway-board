baea79a973252a1e648caf1c3678d9e041d27bf0
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-19 02:55:03 +0900

    async-await を利用したが、かえって読み辛い気がしたので不採用だが試行錯誤の跡として残す。

--- src/hooks/useFetchApi.js
+++ src/hooks/useFetchApi.js
@@ -13,10 +13,36 @@ export const useFetchApi = () => {
         setThreads(data);
       })
       .catch(error => {
-        if (error.message.charAt(0) == 4) alert("無効な URLです");
-        if (error.message.charAt(0) == 5) alert("サーバー通信でエラーが発生しました。");
+        if (error.message.charAt(0) == 4) return alert("無効な URLです");
+        if (error.message.charAt(0) == 5) return alert("ブラウザをリロードしてみて下さい。");
       });
   }, []);
 
   return threads;
 };
+
+// await を用いてみたが、useEffect の内部で非同期を扱うとかえって読みにくいのでやめた。
+
+// export const useFetchApi = () => {
+//   const [threads, setThreads] = useState([]);
+
+//   useEffect(() => {
+//     (async () => {
+//       try {
+//         const data = await fetchThreadDatas()
+//         setThreads(data)
+//       } catch (error) {
+//         if (error.message.charAt(0) == 4) alert("無効な URLです");
+//         if (error.message.charAt(0) == 5) alert("サーバー通信でエラーが発生しました。")
+//       }
+//     })();
+//   }, []);
+
+//   return threads;
+// };
+
+// const fetchThreadDatas = async () => {
+//   const response = await fetch('https://railway-react-bulletin-board.herokuapp.com/threads', { method: 'GET' });
+//   if (!response.ok) throw new Error(response.status)
+//   return await response.json();
+// }
