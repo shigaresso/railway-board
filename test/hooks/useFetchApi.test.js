a5f9ee610c874717a21109d805ed289a18a83734
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-20 14:30:08 +0900

    非同期で例外を投げるユニットテストの作成が完了した。
    TODO：現在のテスト方法だとネットワーク接続が必要になり、コストが高くなると思われるので、モック化させる。

--- test/hooks/useFetchApi.test.js
+++ test/hooks/useFetchApi.test.js
@@ -4,55 +4,54 @@ describe("掲示板一覧を取得する GET メソッドのテスト", () => {
   it("GET メソッド成功時に対しての記述", () => {
     const threadDatas = [
       {
-          "id": "4",
-          "title": "好きなプログラミング言語"
+        "id": "4",
+        "title": "好きなプログラミング言語"
       },
       {
-          "id": "14",
-          "title": "あなたのGitHub"
+        "id": "14",
+        "title": "あなたのGitHub"
       },
       {
-          "id": "24",
-          "title": "railwayについて"
+        "id": "24",
+        "title": "railwayについて"
       },
       {
-          "id": "34",
-          "title": "hoge"
+        "id": "34",
+        "title": "hoge"
       },
       {
-          "id": "44",
-          "title": "hoge2"
+        "id": "44",
+        "title": "hoge2"
       },
       {
-          "id": "54",
-          "title": "hoge3"
+        "id": "54",
+        "title": "hoge3"
       },
       {
-          "id": "64",
-          "title": "hoge4"
+        "id": "64",
+        "title": "hoge4"
       },
       {
-          "id": "74",
-          "title": "hoge5"
+        "id": "74",
+        "title": "hoge5"
       },
       {
-          "id": "84",
-          "title": "こんにちは世界"
+        "id": "84",
+        "title": "こんにちは世界"
       },
       {
-          "id": "94",
-          "title": "aaaaa"
+        "id": "94",
+        "title": "aaaaa"
       }
-  ]
+    ]
     return fetchThreadDatas('https://railway-react-bulletin-board.herokuapp.com/threads').then(data => {
       expect(data).toEqual(threadDatas);
     });
   });
 
-  // it('GET メソッドの URL を間違えた際の処理', async () => {
-  //   expect(() => await fetchThreadDatas('https://railway-react-bulletin-board.herokuapp.com/threadsb')).toThrow()
-  // //    .then(data => {
-  // //     expect(data).toEqual(threadDatas);
-  // // })
-  // })
+  it('GET メソッドの URL を間違えた際の処理', () => {
+    expect(() => fetchThreadDatas('https://railway-react-bulletin-board.herokuapp.com/threadsb'))
+      .rejects.toThrow("404");
+    // ↑ 非同期処理の場合、 と .Throw() の前に .rejects を付け加える必要がある
+  });
 });
