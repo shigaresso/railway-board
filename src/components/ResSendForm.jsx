944f172c6ae5d8908951e2dfac4e864d7b521bbb
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-22 01:13:25 +0900

    POST メソッドで書き込めるようにした。

--- src/components/ResSendForm.jsx
+++ src/components/ResSendForm.jsx
@@ -3,24 +3,29 @@ import axios from "axios";
 import resSendForm from "./ResSendForm.module.css"
 
 export const ResSendForm = (props) => {
-  const postUrl = `https://railway-react-bulletin-board.herokuapp.com/threads/${props.threadid}/posts`;
+  const postUrl = `https://railway-react-bulletin-board.herokuapp.com/threads/${props.threadId}/posts`;
   const [resContent, setResContent] = useState("");
 
-  const sendRes = async (postUrl, resContent) => {
+  const sendRes = async (event) => {
+    event.preventDefault();
     const body = {
       post: resContent
     }
     await axios.post(postUrl, body)
+      .then(_ =>
+        setResContent("")
+      )
       .catch(error => {
         throw new Error(error.request.status);
       });
+    await props.fetchData()
   }
 
   return (
     <div className={resSendForm.create_new_thread}>
-      <form onSubmit={() => window.location.reload()}>
-        <input className={resSendForm.res_content} type="text" value={resContent} onChange={(event) => { setResContent(event.target.value) }} placeholder="スレッドタイトル" />
-        <button className={resSendForm.form_button} onClick={async () => await sendRes(postUrl, resContent)}>投稿</button>
+      <form onSubmit={async (event) => await sendRes(event)}>
+        <input className={resSendForm.res_content} type="text" value={resContent} onChange={(event) => { setResContent(event.target.value) }} placeholder="投稿しよう！" />
+        <button className={resSendForm.form_button} type={'submit'}>投稿</button>
       </form>
     </div>
   );
