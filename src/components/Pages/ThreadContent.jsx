32d7bcffb6afd6e778d1f422280749050c6cebf8
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-22 01:15:56 +0900

    ResSendForm で POST メソッド時に更新処理をするようにした。
    更新処理は、ブラウザのリロードではなく、非同期通信でネットワークから取得する。
    これは SPA で差分更新をする React の利点である。

--- src/components/Pages/ThreadContent.jsx
+++ src/components/Pages/ThreadContent.jsx
@@ -9,32 +9,54 @@ export const ThreadContent = () => {
   // 分割代入で値を取得することに注意する
   const { threadId } = useParams();
   const [threads, setThreads] = useState([]);
+  const [firstMessage, setFirstMessage] = useState(0);
+
+  const disableNextPageButton = threads?.posts?.length != 10 ? true : false;
+  const disablePreviewPageButton = firstMessage - 10 <= -1 ? true : false;
+
+  async function fetchData() {
+    const response = await fetchThreadDatas(`https://railway-react-bulletin-board.herokuapp.com/threads/${threadId}/posts?offset=${firstMessage}`)
+      .catch(error => {
+        if (error.message.charAt(0) == 4) return alert("無効な URLです");
+        if (error.message.charAt(0) == 5) return alert("ブラウザをリロードしてみて下さい。");
+      });
+
+    setThreads(response);
+  }
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetchThreadDatas(`https://railway-react-bulletin-board.herokuapp.com/threads/${threadId}/posts?offset=0`)
-        .catch(error => {
-          if (error.message.charAt(0) == 4) return alert("無効な URLです");
-          if (error.message.charAt(0) == 5) return alert("ブラウザをリロードしてみて下さい。");
-        });
-
-      setThreads(response);
-    }
     fetchData();
   }, []);
+
+  const changePage = async () => {
+    const response = await fetchThreadDatas(`https://railway-react-bulletin-board.herokuapp.com/threads/${threadId}/posts?offset=${firstMessage}`)
+      .catch(error => {
+        if (error.message.charAt(0) == 4) return alert("無効な URLです");
+        if (error.message.charAt(0) == 5) return alert("ブラウザをリロードしてみて下さい。");
+      });
+
+    setThreads(response);
+  }
+
+  // 関数内部で useState の更新関数を呼び出すとうまく変更されないので、useState の値が変更されると取得するスレッドの最初のメッセージ番号を変更するようにした。
+  useEffect(() => {
+    changePage();
+  }, [firstMessage])
 
   return (
     <main className={threadContent.container}>
       <div className={threadContent.main_container}>
         <h1 className={threadContent.thread_name}>{threads.title}</h1>
-        <div>
-          {/* posts を posts? としないとサーバーからデータを受け取る前に map を実行してしまいエラーになる */}
-          {threads.posts?.map(res => <Res key={res.id} content={res.post} />)}
+        {/* posts を posts? としないとサーバーからデータを受け取る前に map を実行してしまいエラーになる */}
+        {threads.posts?.map(res => <Res key={res.id} content={res.post} />)}
+        <div className={threadContent.change_page}>
+          <button disabled={disablePreviewPageButton} onClick={() => setFirstMessage(firstMessage - 10)}>前のページへ</button>
+          <button disabled={disableNextPageButton} onClick={() => setFirstMessage(firstMessage + 10)}>次のページへ</button>
         </div>
       </div>
-      
+
       <div className={threadContent.res_container}>
-        <ResSendForm />
+        <ResSendForm threadId={threadId} fetchData={fetchData} />
       </div>
     </main>
   );
