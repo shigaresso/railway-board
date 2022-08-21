365037eac716c70634628a660da060dbecc994ff
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-21 16:19:00 +0900

    Home の CSS をモジュール化した。

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,16 +1,18 @@
 import { Thread } from "../Thread/Thread"
 import { useFetchApi } from "../../hooks/useFetchApi";
-import './Home.css';
+import home from './Home.module.css';
 
 export const Home = () => {
   // alert はこちら側で呼ぶようにする
   const threads = useFetchApi('https://railway-react-bulletin-board.herokuapp.com/threads');
 
   return (
-    <div className="main-container">
-      <h1 className="new-thread">新着スレッド</h1>
-      <div className="threads">
-        {threads.map(thread => <Thread key={thread.id} id={thread.id} title={thread.title} />)}
+    <div className={home.home_container}>
+      <div className={home.home_container}>
+        <h1 className={home.new_thread}>新着スレッド</h1>
+        <div>
+          {threads.map(thread => <Thread key={thread.id} id={thread.id} title={thread.title} />)}
+        </div>
       </div>
     </div>
   );
