5653a7198dc44274707defba046f8c3174db3ea8
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-08-21 15:56:58 +0900

    ヘッダーの CSS をモジュール化

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
-import './Header.css';
+import header from './Header.module.css';
 
 export const Header = () => {
   return (
-    <header className="app-header">
-      <h1 className='header-content'>掲示板</h1>
-      <Link to="/thread/new" className='header-content'>スレッドをたてる</Link>
+    <header className={header.app_header}>
+      <h1 className={header.header_content}>掲示板</h1>
+      <Link to="/thread/new" className={header.header_content}>スレッドをたてる</Link>
     </header>
   );
 };
